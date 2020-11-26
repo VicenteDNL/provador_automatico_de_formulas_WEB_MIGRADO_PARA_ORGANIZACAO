810247c608236450f131cadb9301eb38fadfe093
Danilo <danilo.saraiva68@gmail.com>
2020-11-25 22:46:41 -0300

    alterado link da api para https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   // production: false,
   // API:'http://localhost:8000/api/'
   production: true,
-  API:'http://api-arvore-refutacao.thelogiclive.com/api/'
+  API:'https://api-arvore-refutacao.thelogiclive.com/api/'
 };
