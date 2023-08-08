3df511e8b4cbb06928fb47da37ba4e60d8811dbb
Danilo <danilo.saraiva68@gmail.com>
2023-08-07 21:43:38 -0300

    feat(model):no

---
+++ src/app/common/models/arvore/nos.ts
@@ -0,0 +1,21 @@
+export type No =  {
+
+    // arv: {};
+    str:  string;
+    idNo: number;
+    linha: number;
+    noFolha: boolean;
+    posX: number;
+    posY: number;
+    tmh: number;
+    posXno: number;
+    linhaDerivacao: number|null;
+    posXlinhaDerivacao: number;
+    utilizado: boolean;
+    fechado: boolean;
+    linhaContradicao: number|null;
+    fill: string;
+    strokeWidth: number;
+    strokeColor: string;
+
+};
