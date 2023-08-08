30b88ee5225f737eef518185e038bc076b966892
Danilo <danilo.saraiva68@gmail.com>
2023-08-07 22:51:07 -0300

    feat(model): models

---
+++ src/app/common/models/tentativas.ts
@@ -0,0 +1,11 @@
+export type Tentativas = {
+    erros: number;
+    pontuacao: {
+        ponto: number;
+        maximo: number;
+    };
+    tempo: {
+        minutos: number;
+        segundos: number;
+    };
+};
