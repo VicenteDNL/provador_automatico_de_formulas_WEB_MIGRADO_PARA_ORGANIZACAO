06e9458ef13302509c57773622472a37fe310f74
Danilo <danilo.saraiva68@gmail.com>
2023-08-14 23:53:42 -0300

    feat(visualizador): ajusta

--- src/app/exercicios/components/vizualizador-arvore/vizualizador-arvore.component.ts
+++ src/app/exercicios/components/vizualizador-arvore/vizualizador-arvore.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Aresta } from 'src/app/common/models/arvore/aresta.model';
+import { No } from 'src/app/common/models/arvore/no.model.';
 
 @Component({
   selector: 'app-vizualizador-arvore',
@@ -6,21 +8,38 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./vizualizador-arvore.component.css'],
 })
 export class VizualizadorArvoreComponent implements OnInit {
-  @Input() impressaoNo: Array<any>;
-  @Input() impressaoAresta: Array<any>;
+  @Input() impressaoNo: No[];
+  @Input() impressaoAresta: Aresta[];
   @Input() exibirLinha: boolean;
   @Input() width: number;
   @Input() height: number;
   fillColor = 'url(#grad1)';
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if(this.exibirLinha){
+      const comprimentoTextoDaLinha=200;
+      const deslocamento=comprimentoTextoDaLinha/2;
+      this.width= this.width + comprimentoTextoDaLinha;
+      this.impressaoNo =this.impressaoNo.map((no)=>{
+        no.posXno=no.posXno+deslocamento;
+        no.posX=no.posX+deslocamento;
+        no.posXlinhaDerivacao=no.posXlinhaDerivacao+deslocamento;
+        return no;
+      });
+      this.impressaoAresta =this.impressaoAresta.map((aresta)=>{
+        aresta.linhaX1=aresta.linhaX1+deslocamento;
+        aresta.linhaX2=aresta.linhaX2+deslocamento;
+        return aresta;
+      });
+    }
+  }
 
-  alterarcor(index) {
+  alterarcor(index: number) {
     this.impressaoNo[index].fill = 'url(#grad2)';
   }
 
-  voltarcor(index) {
+  voltarcor(index: number) {
     this.impressaoNo[index].fill = 'url(#grad1)';
   }
 }
