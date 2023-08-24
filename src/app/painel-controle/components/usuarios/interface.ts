211136e8ad94c17060ed00e8066530d72a1447a5
Danilo <danilo.saraiva68@gmail.com>
2023-08-23 23:23:04 -0300

    feat(user, crud):created crud user

--- src/app/painel-controle/components/usuarios/interface.ts
+++ src/app/painel-controle/components/usuarios/interface.ts
@@ -3,27 +3,26 @@ import { BaseResponse } from 'src/app/common/models/baseResponse.model';
 import { PaginationResponse } from 'src/app/common/models/paginationResponse.model';
 
 export type Response = BaseResponse;
-export interface Usuario{
-    id: number;
-    nome: string;
-    email: string;
-    email_verificado: string;
-    created_at: string;
-    updated_at: string;
+export interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+  email_verificado: string;
+  created_at: string;
+  updated_at: string;
 }
 
-export interface UsuarioResponse extends Response{
+export interface UsuarioResponse extends Response {
   data?: Usuario;
 }
 
-export interface UsuarioInput{
-
-    id?: number;
-    nome: string;
-    email: string;
-    password: string;
-    ativo: boolean;
+export interface UsuarioInput {
+  id?: number;
+  nome: string;
+  email: string;
+  password: string;
+  ativo: boolean;
 }
-export interface UsuarioPaginationResponse extends Response{
+export interface UsuarioPaginationResponse extends Response {
   data?: PaginationResponse<Usuario[]>;
-};
+}
--- src/app/painel-controle/components/usuarios/usuarios.service.ts
+++ src/app/painel-controle/components/usuarios/usuarios.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Response, UsuarioInput, UsuarioPaginationResponse, UsuarioResponse } from './interface';
+import {
+  Response,
+  UsuarioInput,
+  UsuarioPaginationResponse,
+  UsuarioResponse,
+} from './interface';
 import { take } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuariosService {
   private readonly api = `${environment.api}usuarios`;
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   listar(pg: number) {
     return this.http.get<UsuarioPaginationResponse>(`${this.api}?page=${pg}`);
@@ -31,5 +35,4 @@ export class UsuariosService {
   editar(user: UsuarioInput) {
     return this.http.put<Response>(`${this.api}/${user.id}/`, user);
   }
-
 }
