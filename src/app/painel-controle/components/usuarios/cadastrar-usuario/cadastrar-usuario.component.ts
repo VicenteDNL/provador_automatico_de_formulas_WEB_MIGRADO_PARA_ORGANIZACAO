9d858f04cd67e46eea7cc16d5c7cb197a8af023d
Danilo <danilo.saraiva68@gmail.com>
2023-08-23 23:22:16 -0300

    feat(user, crud):created crud user

--- src/app/painel-controle/components/usuarios/cadastrar-usuario/cadastrar-usuario.component.ts
+++ src/app/painel-controle/components/usuarios/cadastrar-usuario/cadastrar-usuario.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { UsuarioInput, UsuarioResponse } from '../interface';
 import { UsuariosService } from '../usuarios.service';
 import { Router } from '@angular/router';
@@ -8,20 +7,16 @@ import { PainelControleComponent } from 'src/app/painel-controle/painel-controle
 @Component({
   selector: 'app-cadastrar-usuario',
   templateUrl: './cadastrar-usuario.component.html',
-  styleUrls: ['./cadastrar-usuario.component.css']
+  styleUrls: ['./cadastrar-usuario.component.css'],
 })
 export class CadastrarUsuarioComponent implements OnInit {
-
-  iconVoltar = faArrowAltCircleLeft;
-
   usuario: UsuarioInput = {
-    nome:'',
-    email:'',
-    password:'',
-    ativo:true
+    nome: '',
+    email: '',
+    password: '',
+    ativo: true,
   };
   requisitando = false;
-  spineer = false;
 
   constructor(
     private service: UsuariosService,
@@ -29,31 +24,23 @@ export class CadastrarUsuarioComponent implements OnInit {
     private painelCmp: PainelControleComponent,
   ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   cadastrarUsuario() {
     this.requisitando = true;
-    this.spineer = true;
-
     this.service.cadastrar(this.usuario).subscribe(
-      response => this.sucessoCadastro(response),
-      error => this.errorCadastro(error),
+      response => {
+        this.requisitando = false;
+        if (response.success === true) {
+          this.router.navigate(['/painel/usuarios']);
+        } else {
+          this.painelCmp.errorMensagen = response.msg;
+        }
+      },
+      error => {
+        this.painelCmp.errorMensagen =
+          error.error.msg ?? 'Ocorreu um problema ao executar esta ação';
+      },
     );
-  }
-
-  sucessoCadastro(response: UsuarioResponse) {
-    this.requisitando = false;
-    this.spineer = false;
-    if (response.success === true) {
-      this.router.navigate(['/painel/usuarios']);
-    } else {
-      this.painelCmp.errorMensagen = response.msg;
-    }
-  }
-
-  errorCadastro(response: any) {
-    this.spineer = false;
-    this.painelCmp.errorMensagen = response.error.msg ?? 'Ocorreu um problema ao executar esta ação';
   }
 }
--- src/app/painel-controle/components/usuarios/editar-usuario/editar-usuario.component.ts
+++ src/app/painel-controle/components/usuarios/editar-usuario/editar-usuario.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { UsuarioInput, UsuarioResponse } from '../interface';
 import { UsuariosService } from '../usuarios.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,21 +8,17 @@ import { map, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-editar-usuario',
   templateUrl: './editar-usuario.component.html',
-  styleUrls: ['./editar-usuario.component.css']
+  styleUrls: ['./editar-usuario.component.css'],
 })
 export class EditarUsuarioComponent implements OnInit {
-
-  iconVoltar = faArrowAltCircleLeft;
-
   usuario: UsuarioInput = {
-    id:null,
-    nome:'',
-    email:'',
-    password:'',
-    ativo:true
+    id: null,
+    nome: '',
+    email: '',
+    password: '',
+    ativo: true,
   };
   requisitando = false;
-  spineer = false;
 
   constructor(
     private service: UsuariosService,
@@ -38,7 +33,6 @@ export class EditarUsuarioComponent implements OnInit {
         map((params: any) => params.id),
         switchMap(id => {
           this.requisitando = true;
-          this.spineer = true;
           return this.service.buscar(id);
         }),
       )
@@ -46,48 +40,39 @@ export class EditarUsuarioComponent implements OnInit {
         response => this.carregarUsuario(response),
         error => {
           this.requisitando = false;
-          this.errorEdicao(error);
+          this.painelCmp.errorMensagen =
+            error.error.msg ?? 'Ocorreu um problema ao executar esta ação';
         },
       );
   }
 
-  carregarUsuario(response: UsuarioResponse){
-
-    if(response.success){
+  carregarUsuario(response: UsuarioResponse) {
+    if (response.success) {
       this.usuario.id = response.data.id;
-      this.usuario.nome =response.data.nome;
-      this.usuario.email =response.data.email;
+      this.usuario.nome = response.data.nome;
+      this.usuario.email = response.data.email;
       this.usuario.password = '';
-    }else{
+    } else {
       this.painelCmp.errorMensagen = response.msg;
     }
     this.requisitando = false;
-    this.spineer = false;
   }
 
   editarUsuario() {
     this.requisitando = true;
-    this.spineer = true;
-
     this.service.editar(this.usuario).subscribe(
-      response => this.sucessoEdicao(response),
-      error => this.errorEdicao(error),
+      response => {
+        this.requisitando = false;
+        if (response.success === true) {
+          this.router.navigate(['/painel/usuarios']);
+        } else {
+          this.painelCmp.errorMensagen = response.msg;
+        }
+      },
+      error => {
+        this.painelCmp.errorMensagen =
+          error.error.msg ?? 'Ocorreu um problema ao executar esta ação';
+      },
     );
   }
-
-  sucessoEdicao(response: UsuarioResponse) {
-    this.requisitando = false;
-    this.spineer = false;
-    if (response.success === true) {
-      this.router.navigate(['/painel/usuarios']);
-    } else {
-      this.painelCmp.errorMensagen = response.msg;
-    }
-  }
-
-  errorEdicao(response: any) {
-    this.spineer = false;
-    this.painelCmp.errorMensagen = response.error.msg ?? 'Ocorreu um problema ao executar esta ação';
-  }
-
 }
